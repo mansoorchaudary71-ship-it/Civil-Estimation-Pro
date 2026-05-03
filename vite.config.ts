e30d025ea754370ae9a2ad158d8f2724d67c360d
th <mansoorchaudary71@gmail.com>
2026-05-03 22:36:40 +0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig, loadEnv} from 'vite';
 
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
+  base: "/Civil-Estimation-Pro/",
   return {
     plugins: [react(), tailwindcss()],
     define: {
